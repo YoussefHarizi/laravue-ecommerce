74c6e46a4520efc7b41ff112dacc2453042726f5
YoussefHarizi <youssef.harizi@gmail.com>
2023-01-01 10:15:30 -0500

    Authcontroller & admin middleware
    impliment login & logout function in AuthController, add api routes & admi middlewire

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum','admin'])->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/logout',[\App\Http\Controllers\AuthController::class,'logout']);
+
 });
+Route::get('/login',[\App\Http\Controllers\AuthController::class,'login']);
